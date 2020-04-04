3807ebeff09f1becebd13ac795e8dc3493e84e31
David DiGioia <davidofjoy@gmail.com>
2020-04-04 16:00:00 -0400

    Made two python files

---
+++ src/driver.py
@@ -0,0 +1,12 @@
+# This is the driver file, so we can do the startup work of the program here,
+# like instantiating a graph and initializing a window to show graphics in
+
+
+def main():
+    pass
+
+
+# run the driver.py function only if this module is executed as the driver.py script
+# (if you import this as a module then nothing is executed)
+if __name__ == "__main__":
+    main()
---
+++ src/graph.py
@@ -0,0 +1,5 @@
+# Here we can write the graph data structure and all the functionality related to it
+
+
+class Graph:
+    pass
