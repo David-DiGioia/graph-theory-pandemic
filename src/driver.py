a8830a4334f5662e802dd606ef26c2d52fa1ad44
David DiGioia <davidofjoy@gmail.com>
2020-04-07 12:30:14 -0400

    started implementing vertex and graph classes

--- src/driver.py
+++ src/driver.py
@@ -1,9 +1,21 @@
 # This is the driver file, so we can do the startup work of the program here,
 # like instantiating a graph and initializing a window to show graphics in
+from graph import *
 
 
 def main():
-    pass
+    # Make new graph
+    graph = Graph()
+
+    # Populate graph with 5 vertices which are unconnected to each other
+    for i in range(5):
+        graph.add_vertex(Vertex())
+
+    # Make an edge between the vertex with id=1 and the vertex with id=2
+    graph.make_edge(0, 1)
+
+    # Vertex with id=0 has 1 adjacent vertex since we just made an edge connected to it
+    print("Vertex with id=1 has this many adjacent vertices: " + str(len(graph.vertices[0].adjacentVertices)))
 
 
 # run the driver.py function only if this module is executed as the driver.py script
--- src/graph.py
+++ src/graph.py
@@ -1,5 +1,55 @@
-# Here we can write the graph data structure and all the functionality related to it
+# Here we can write any data structures related to graphs and all the functionality that goes with it
+
+
+vertexId = 0
+
+
+class Vertex:
+    def __init__(self, infected=False, adjacent=None):
+        global vertexId
+
+        # Is this vertex infected?
+        self.infected = infected
+
+        # adjacentVertices is a set of all vertices' IDs which are adjacent to this one
+        if adjacent is None:
+            self.adjacentVertices = set()
+        else:
+            self.adjacentVertices = adjacent
+
+        # Assign a unique id to this vertex (so that we can reference it later)
+        self.id = vertexId
+        vertexId += 1
 
 
 class Graph:
-    pass
+    def __init__(self):
+        # vertices is a dictionary associating the each vertex's id number with the vertex
+        self.vertices = {}
+        # frontier is a set containing all vertices which have potential to infect other vertices.
+        # That is, it contains each infected vertex with uninfected adjacent vertices
+        self.frontier = set()
+
+    def add_vertex(self, vertex):
+        # Make the vertex's id number the key to accessing the vertex in the dictionary
+        self.vertices[vertex.id] = vertex
+
+    def make_edge(self, v1, v2):
+        # Since this is an undirected graph, we add the vertices to each other's list of adjacent vertices
+        self.vertices[v1].adjacentVertices.add(v2)
+        self.vertices[v2].adjacentVertices.add(v1)
+
+    def update_frontier(self):
+        for vertex in self.frontier:
+            for adjacent in vertex.adjacentVertices:
+                if not adjacent.infected:
+                    break
+            # If all vertices adjacent to vertex are infected already, remove vertex from the frontier
+            self.frontier.remove(vertex)
+
+    def infect_vertex(self, vertex):
+        vertex.infected = True
+        self.update_frontier()
+
+    def get_vertex(self, id):
+        return self.vertices[id]
